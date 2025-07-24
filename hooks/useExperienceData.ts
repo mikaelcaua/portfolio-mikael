6e3311a03acd3e4128c9c3c2d8eac8cbd1d8b5bb
mikaelcaua <mikaelcauasilva1@gmail.com>
2025-07-24 20:32:19 -0300

    fix: update experience data formatting

--- hooks/useExperienceData.ts
+++ hooks/useExperienceData.ts
@@ -5,36 +5,36 @@ export function useExperienceData(): ExperienceSectionProps {
     experiences: [
       {
         company: "Telessaúde Maranhão",
-        position: "Desenvolvedor Full Stack (Atual)",
-        period: "Maio 2025 Atual",
+        position: "Desenvolvedor Full Stack",
+        period: "Maio 2025 - Atual",
         description:
           "Atuação no desenvolvimento de soluções tecnológicas para a área da saúde, com manutenção de sistemas legados e criação de novos sistemas usando React, Next.js, PHP Laravel e React Native.",
       },
       {
-        company: "Alura",
-        position: "Instrutor",
-        period: "Ago 2024 Maio 2025",
-        description:
-          "Criação de conteúdo didático em Dart, Flutter, React Native e Expo. Desenvolvimento de cursos, artigos e atividades para o ensino de tecnologia, com foco em boas práticas e metodologias ágeis.",
-      },
-      {
         company: "UFMA",
         position: "Maratonista de Programação pela SBC",
-        period: "Set 2023 Atual",
+        period: "Set 2023 - Atual",
         description:
           "Desenvolvimento de soluções algorítmicas otimizadas em Python para competições da SBC. Análise de complexidade de algoritmos e implementação de estruturas de dados complexas.",
       },
       {
+        company: "Alura",
+        position: "Instrutor",
+        period: "Ago 2024 - Maio 2025",
+        description:
+          "Criação de conteúdo didático em Dart, Flutter, React Native e Expo. Desenvolvimento de cursos, artigos e atividades para o ensino de tecnologia, com foco em boas práticas e metodologias ágeis.",
+      },
+      {
         company: "Pulse",
         position: "Jovem Tech - Bolsista",
-        period: "Jan 2024 Jul 2024",
+        period: "Jan 2024 - Jul 2024",
         description:
           "Desenvolvimento frontend com HTML, CSS, JavaScript, React e Next.js.",
       },
       {
         company: "PETComp",
         position: "Monitor e Desenvolvedor",
-        period: "Abr 2023 Jan 2024",
+        period: "Abr 2023 - Jan 2024",
         description:
           "Desenvolvimento mobile com Expo, automação com Google Apps Script e monitorias acadêmicas em Algoritmos, Matemática Discreta, Lógica e Cálculo.",
       },
